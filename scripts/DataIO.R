773cb006020e9d3be9ce2cb3dbbbb384297f6141
Tajrian Rahman <tajrianrahman311@gmail.com>
2022-12-31 20:18:59 +0600

    vector added

--- scripts/DataIO.R
+++ scripts/DataIO.R
@@ -1,11 +1,11 @@
-# Load data 
+#Load data     
 library(gapminder)
 library(skimr)
-
+   
 # Summarize data 
 skim(gapminder)
-
-# Summarize air quality  
+ 
+# Summarize air quality   
 skim(airquality)
 
 
@@ -14,3 +14,6 @@ skim(gapminder)
 
 
 str(gapminder)
+
+v1 <-c(1,2,3,4)
+v1
